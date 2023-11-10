d9da29ad07bbb446e7357cdcd6dc089ef8ebe4f9
MKM12345 <120132998+MKM12345@users.noreply.github.com>
2023-11-10 19:18:08 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, send_file
 import requests
+from io import BytesIO
 
 app = Flask(__name__)
 
@@ -14,6 +15,12 @@ def index():
             response = requests.get(url)
             if response.status_code == 200:
                 page_content = response.text
+                # Save the content to a file in memory
+                file = BytesIO(page_content.encode())
+                file.seek(0)
+
+                # Send the content as a file download
+                return send_file(file, as_attachment=True, download_name='downloaded_page.txt', mimetype='text/plain')
             else:
                 page_content = f"Failed to download the web page. Status code: {response.status_code}"
         except Exception as e:
